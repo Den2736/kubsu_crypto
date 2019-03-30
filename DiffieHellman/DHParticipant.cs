d52b6765de025b90ccf24e3a4e14fd1411d6c72d
den2736 <babencko.denis2011@yandex.ru>
2019-03-31 00:22:21 +0300

    DiffieHellman

--- DiffieHellman/DHParticipant.cs
+++ DiffieHellman/DHParticipant.cs
@@ -1,53 +1,78 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 
 namespace DiffieHellman {
     public class DiffieHellmanParticipant {
         public string Name { get; set; }
         public int Base { get; set; }
-        public uint Mod { get; set; }
+        public int Mod { get; set; }
+        public BigInteger PartnerMix { get; set; }
+        private string _log { get; set; }
 
         private int _secret { get; set; }
-        private int _mix { get; set; }
-        public int PartnerMix { get; set; }
-        private int _sharedKey { get; set; }
+        private BigInteger _mix { get; set; }
+        private BigInteger _sharedKey { get; set; }
 
-        public DiffieHellmanParticipant (string name) => Name = name;
-
-        public void ShareGeneratorsWith (DiffieHellmanParticipant partner) {
-            partner.Base = Base;
-            partner.Mod = Mod;
+        public DiffieHellmanParticipant(string name) {
+            Name = name;
+            _log = $"{Name}.log";
+            File.WriteAllText(_log, "");
         }
 
-        public void GenerateSecret () {
-            _secret = new Random ().Next (0, int.MaxValue);
+        public void BroadcastGeneratorsIn(string filename) {
+            File.AppendAllLines(filename, new string[] {
+                "Base = " + Base.ToString(),
+                    "Mod = " + Mod.ToString()
+            });
+        }
+        public void GetGeneratorsFrom(string filename) {
+            var lines = File.ReadAllLines(filename);
+            string baseLine = lines.First(s => s.ToLower().Contains("base"));
+            string modLine = lines.First(s => s.ToLower().Contains("mod"));
+
+            Base = int.Parse(baseLine.Split('=').Last());
+            Mod = int.Parse(modLine.Split('=').Last());
+            Log($"Received base({Base}) and mod({Mod})");
         }
 
-        public void Mix () {
-            _mix = (int) (Math.Pow (Base, _secret) % Mod);
+        public void GenerateSecret() {
+            _secret = new Random().Next(0, 10000);
+            Log($"My secret: {_secret}");
         }
 
-        public void ShareMixWith (DiffieHellmanParticipant partner) {
-            partner.PartnerMix = _mix;
+        public void Mix() {
+            _mix = BigInteger.Pow(Base, _secret) % Mod;
+            Log($"My mix: {_mix}");
         }
 
-        public void GenerateSharedKey () {
-            _sharedKey = (int) (Math.Pow (PartnerMix, _secret) % Mod);
-            Log($"SharedKey = {_sharedKey}");
+        public void BroadcastMixIn(string filename) {
+            File.AppendAllText(filename, $"[{Name}].Mix = {_mix}{Environment.NewLine}");
+        }
+        public void GetPartnersMixFrom(string filename) {
+            var lines = File.ReadAllLines(filename);
+            var reMix = new Regex(@"\[\w+].Mix");
+            var reName = new Regex(@"\[\w+]");
+
+            var lineWithPartnerMix = lines.First(s =>
+                reMix.Matches(s).Any()
+                && reName.Match(s).Value.Replace("[", "").Replace("]", "") != Name);
+            PartnerMix = BigInteger.Parse(lineWithPartnerMix.Split('=').Last().Trim());
+            Log($"Partner's mix: {PartnerMix}");
         }
 
-        public bool Trusts (DiffieHellmanParticipant partner) {
-            return Enumerable.SequenceEqual (GetSharedSecretHash (), partner.GetSharedSecretHash ());
+        public void GenerateSharedKey() {
+            _sharedKey = BigInteger.Pow(PartnerMix, _secret) % Mod;
+            Log($"SharedKey: {_sharedKey}");
         }
-        public byte[] GetSharedSecretHash () {
-            using (var md5 = MD5.Create ()) {
-                return md5.ComputeHash (BitConverter.GetBytes (_sharedKey));
-            }
-        }
-        private void Log (string message) {
-            File.AppendAllText ($"{Name}.log", $"{DateTime.Now}: {message}");
+
+        public override string ToString() => Name;
+
+        private void Log(string message) {
+            File.AppendAllText(_log, message + Environment.NewLine);
         }
     }
 }
---
+++ DiffieHellman/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using PrimeGenerator = PrimeNumberGenerator.PrimeNumberGenerator;
+
+namespace DiffieHellman {
+    class Program {
+        static DiffieHellmanParticipant cp; // current participant
+        static string partnerName;
+        static readonly string channel = "channel.txt";
+        static readonly DirectoryInfo cd = new DirectoryInfo(Environment.CurrentDirectory);
+        static void Main(string[] args) {
+            Clear();
+
+            Console.WriteLine("Hello, Diffie-Hellman participant!");
+            Console.WriteLine("What's your name?");
+
+            string name = Console.ReadLine();
+            cp = new DiffieHellmanParticipant(name);
+
+            Console.Write($"What do you want to do? 1 - if you want to start to communicate, or 2 - if someone wants to communicate with you:");
+            string choice = Console.ReadLine();
+
+            partnerName = FindPartners().FirstOrDefault(p => p != cp.Name);
+
+            while (partnerName == null) {
+                Console.WriteLine($"It looks like there is no one you could contact");
+                Console.WriteLine("Press any key to seek one more time.");
+                Console.ReadKey();
+                partnerName = FindPartners().FirstOrDefault(p => p != cp.Name);
+            }
+
+            if (choice == "1") {
+                Console.WriteLine($"Press any key to start communicate with {partnerName}");
+                Console.ReadKey();
+
+                cp.Base = new Random().Next(0, 10000);
+                cp.Mod = (int) new PrimeGenerator().Generate();
+                cp.BroadcastGeneratorsIn(channel);
+                Console.WriteLine("Generators was shared.");
+            }
+            else {
+                Console.WriteLine($"Press any key to communicate with {partnerName}");
+                Console.ReadKey();
+
+                cp.GetGeneratorsFrom(channel);
+                Console.WriteLine("Generators was received.");
+            }
+            cp.GenerateSecret();
+            Console.WriteLine($"Secret was generated.");
+
+            cp.Mix();
+            Console.WriteLine($"Mixed.");
+
+            cp.BroadcastMixIn(channel);
+            Console.WriteLine($"Mix was shared.");
+
+            Console.WriteLine("Press any key to get partner's mix:");
+            Console.ReadKey();
+
+            cp.GetPartnersMixFrom(channel);
+            Console.WriteLine($"Partner's mix was received.");
+
+            cp.GenerateSharedKey();
+            Console.WriteLine($"Got shared key!");
+        }
+
+        static void Clear() {
+            var cd = new DirectoryInfo(Environment.CurrentDirectory);
+            cd.GetFiles("*.log").ToList().ForEach(f => f.Delete());
+            cd.GetFiles("*.txt").ToList().ForEach(f => f.Delete());
+
+            File.WriteAllText(channel, "");
+        }
+        static IEnumerable<string> FindPartners() {
+            return cd.GetFiles("*.log").Select(f => f.Name.Split('.').First().Trim());
+        }
+    }
+}
